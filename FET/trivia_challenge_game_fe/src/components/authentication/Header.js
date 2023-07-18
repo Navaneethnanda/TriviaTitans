c3601bc22edd7a81db29760e778e5d1e8053b100
Jayant <patidar.jayant04@gmail.com>
2023-07-18 12:48:50 -0300

    modified navigation

--- FET/trivia_challenge_game_fe/src/components/authentication/Header.js
+++ FET/trivia_challenge_game_fe/src/components/authentication/Header.js
@@ -1,31 +1,34 @@
-import { HomeIcon, MagnifyingGlassIcon, UserCircleIcon, Bars3Icon } from '@heroicons/react/24/solid';
+import {
+  HomeIcon,
+  MagnifyingGlassIcon,
+  UserCircleIcon,
+  Bars3Icon,
+} from "@heroicons/react/24/solid";
 
-import {  Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div>
-      <div className='bg-[#C1292E] shadow-sm py-4 px-10 flex justify-between items-center'>
-
-
+      <div className="bg-[#C1292E] shadow-sm py-4 px-10 flex justify-between items-center">
         <Link to="/">
-          <h1 className='text-3xl text-white font-bold flex-1 font-serif cursor-pointer'> Trivia</h1></Link>
+          <h1 className="text-3xl text-white font-bold flex-1 font-serif cursor-pointer">
+            {" "}
+            Trivia
+          </h1>
+        </Link>
 
-        <div className=' space-x-5 hidden sm:flex'>
-          <HomeIcon className='w-7 text-white' />
-          <MagnifyingGlassIcon className='w-7 text-white' />
-          <UserCircleIcon className='w-9 text-white' />
+        <div className=" space-x-5 hidden sm:flex">
+          <Link to="/lobby">
+            <HomeIcon className="w-7 text-white" />
+          </Link>
+          <MagnifyingGlassIcon className="w-7 text-white" />
+          <Link to="/profile">
+            <UserCircleIcon className="w-9 text-white" />
+          </Link>
         </div>
-        <Bars3Icon className='w-7 text-white sm:hidden' />
-
-
-
+        <Bars3Icon className="w-7 text-white sm:hidden" />
       </div>
-
-
     </div>
-
-
   );
 }
--- FET/trivia_challenge_game_fe/src/components/authentication/Login.js
+++ FET/trivia_challenge_game_fe/src/components/authentication/Login.js
@@ -1,181 +1,168 @@
-import { useState,useEffect } from "react";
-import { useNavigate, Link } from 'react-router-dom';
-import { auth } from '../../firebase';
-import { onAuthStateChanged,signInWithRedirect,FacebookAuthProvider, signInWithEmailAndPassword, setPersistence, browserLocalPersistence,signInWithPopup,GoogleAuthProvider } from "firebase/auth";
-import { SocialIcon } from 'react-social-icons';
-
-
-
+import { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { auth } from "../../firebase";
+import {
+  onAuthStateChanged,
+  signInWithRedirect,
+  FacebookAuthProvider,
+  signInWithEmailAndPassword,
+  setPersistence,
+  browserLocalPersistence,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { SocialIcon } from "react-social-icons";
 
 export default function Login() {
   const navigate = useNavigate();
   const provider = new GoogleAuthProvider();
   const fprovider = new FacebookAuthProvider();
 
-  const [username, setUsername] = useState('nanda@gmail.com');
-  const [password, setPassword] = useState('Nanda@123');
+  const [username, setUsername] = useState("nanda@gmail.com");
+  const [password, setPassword] = useState("Nanda@123");
 
-
-  useEffect(()=>{
+  useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-        if (user) {
-          navigate("/profile");
-        } 
-      });
-},[]);
+      if (user) {
+        navigate("/lobby");
+      }
+    });
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-
-    const data={username,password}
-    navigate("/SQ",{state:data});
-
-
+    const data = { username, password };
+    navigate("/SQ", { state: data });
   };
 
-
-
-
-  const glogin= ()=>{
-
-console.log("g llogin")
+  const glogin = () => {
+    console.log("g llogin");
     signInWithPopup(auth, provider)
-    .then((result) => {
-      navigate("/profile");
-      // ...
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
-
-
-
+      .then((result) => {
+        navigate("/profile");
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
   };
 
-
-  const flogin=()=>{
+  const flogin = () => {
     console.log("f login");
-   
-
 
     signInWithPopup(auth, fprovider)
-    .then((result) => {
-      // The signed-in user info.
-      const user = result.user;
-  
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      const credential = FacebookAuthProvider.credentialFromResult(result);
-      const accessToken = credential.accessToken;
-  
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = FacebookAuthProvider.credentialFromError(error);
-  
-      // ...
-    });
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user;
 
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const accessToken = credential.accessToken;
 
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = FacebookAuthProvider.credentialFromError(error);
 
-
+        // ...
+      });
   };
-
-
-
-
-
 
   return (
     <div className="flex items-center justify-center ">
-
       <div className="bg-white w-full sm:w-[480px] margin top mt-6 p-8 rounded-md shadow-sm border-[1px]">
-        <div className="font-bold text-2xl mb-4  " >Login</div>
-
-
-
-
-
-
-
-
-
-
-
+        <div className="font-bold text-2xl mb-4  ">Login</div>
 
         <form action="#">
-
           <div className="w-full inline-grid grid-cols-1">
-            <span className="mb-2">Email <span className="text-red-600">*</span></span>
-            <input className="w-full border-gray-200 border-[1px] rounded-sm shadow-sm p-1" type="email" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="johnsmith@hotmail.com" required />
+            <span className="mb-2">
+              Email <span className="text-red-600">*</span>
+            </span>
+            <input
+              className="w-full border-gray-200 border-[1px] rounded-sm shadow-sm p-1"
+              type="email"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="johnsmith@hotmail.com"
+              required
+            />
           </div>
           <div className="my-4 grid grid-cols-1  sm:space-x-4">
             <div className="w-full  inline-grid grid-cols-1">
-              <span className="mb-2">Password <span className="text-red-600">*</span></span>
-              <input className="w-full border-gray-200 border-[1px] rounded-sm shadow-sm p-1" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="********" required />
+              <span className="mb-2">
+                Password <span className="text-red-600">*</span>
+              </span>
+              <input
+                className="w-full border-gray-200 border-[1px] rounded-sm shadow-sm p-1"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="********"
+                required
+              />
             </div>
-
-
           </div>
-
-
 
           <div className="flex   items-center mt-3">
             <div className="bg-[#C1292E] p-3 text-white font-bold text-xl max-w-fit rounded-md cursor-pointer">
-              <input type="submit" value="Login" className="cursor-pointer" onClick={handleSubmit} />
+              <input
+                type="submit"
+                value="Login"
+                className="cursor-pointer"
+                onClick={handleSubmit}
+              />
             </div>
             <div className="ml-auto text-right">
+              <Link to="/Login" className="text-blue-800 underline  block">
+                Forgot Password?
+              </Link>
 
-              <Link to="/Login" className="text-blue-800 underline  block">Forgot Password?</Link>
-
-              <Link to="/Regestration" className="text-blue-800 underline  block">not a user?Sign up</Link>
+              <Link
+                to="/Regestration"
+                className="text-blue-800 underline  block"
+              >
+                not a user?Sign up
+              </Link>
             </div>
           </div>
 
-
-          <div className="flex justify-between  mt-8  " >
-            <button className="flex space-x-3 items-center justify-center text-red-600 max-w-[50%] hover:scale-110 transition-all ease-out " onClick={glogin}>
-
+          <div className="flex justify-between  mt-8  ">
+            <button
+              className="flex space-x-3 items-center justify-center text-red-600 max-w-[50%] hover:scale-110 transition-all ease-out "
+              onClick={glogin}
+            >
               <SocialIcon network="google" style={{ height: 40, width: 40 }} />
               <span className="text-sm">Login with Google</span>
             </button>
 
-            <button className="flex items-center  space-x-3 text-blue-600  max-w-[50%] hover:scale-110 transition-all ease-out" onClick={flogin}>
-
-              <SocialIcon network="facebook"  style={{ height: 40, width: 40 }} />
+            <button
+              className="flex items-center  space-x-3 text-blue-600  max-w-[50%] hover:scale-110 transition-all ease-out"
+              onClick={flogin}
+            >
+              <SocialIcon
+                network="facebook"
+                style={{ height: 40, width: 40 }}
+              />
               <span className="text-sm">Login with Facebook</span>
             </button>
-
           </div>
-
         </form>
-
-        
-
-
-     
-
-
-
-
       </div>
     </div>
-
-
   );
-
-
 }
